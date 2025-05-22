8ae0f2122f13541cca962e9bc459e9997d75cc3b
valimbavaka <christopheric301@gmail.com>
2025-05-22 18:49:33 +0300

    Adding Detail fields

--- Data/DepartementDetails.cs
+++
@@ -1,8 +0,0 @@
-namespace PayApp.Data;
-
-public class DepartementDetails
-{
-    public string Description { get; set; } = string.Empty ;
-    public string NumberOfPost { get; set; } = string.Empty ;
-    public string NumberOfEmployees { get; set; } = string.Empty ;
-}
---
+++ Views/OrgPageComponents/DepartmentDetails.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PayApp.Views.OrgPageComponents;
+
+public partial class DepartmentDetails : UserControl
+{
+    public DepartmentDetails()
+    {
+        InitializeComponent();
+    }
+}
