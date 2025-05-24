b1856d04cf2e09b60a566a01ee4b406d115b2bea
ClaudineRas02 <hanitraclaudine@gmail.com>
2025-05-24 17:01:17 +0300

    affichage avec style non mis au point

---
+++ Dialog/DialogView/AddEmploye.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PayApp.Dialog.DialogView;
+
+public partial class AddEmploye : Window
+{
+    public AddEmploye()
+    {
+        InitializeComponent();
+    }
+}
